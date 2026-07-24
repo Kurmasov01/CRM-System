4fd244e8d55902385695ff153009e96d36ac4326
kurmasov01 <kurmasov01@mail.ru>
2026-07-24 22:06:14 +0500

    ED-2199 / удаление магических чисел validateTodoTitle

--- src/helpers/validateTodoTitle.ts
+++ src/helpers/validateTodoTitle.ts
@@ -1,15 +1,18 @@
 export function validateTodoTitle(title: string | undefined): string {
   const value = title?.trim();
 
+  const titleMaxLength:number = 64;
+  const titleMinLength:number = 2;
+
   if (!value) {
     return "Это поле не может быть пустым";
   }
 
-  if (value.length < 2) {
+  if (value.length < titleMinLength) {
     return "Минимальная длина текста 2 символа";
   }
 
-  if (value.length > 64) {
+  if (value.length > titleMaxLength) {
     return "Максимальная длина текста 64 символа";
   }
 
