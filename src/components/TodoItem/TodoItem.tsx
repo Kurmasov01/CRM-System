5761eaecb08d0c98f5096b5d3dd01db2a7f3c6a8
kurmasov01 <kurmasov01@mail.ru>
2026-07-26 22:29:23 +0500

    ED-2199 / обёрнул редактирование названия задачи в form submit

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -69,74 +69,86 @@ const TodoItem: React.FC<Props> = (props) => {
     setTitle(props.todo.title);
   }
 
+  function handleTitleFormSubmit(event: React.SubmitEvent<HTMLFormElement>) {
+    event.preventDefault();
+    handleSaveTodo(props.todo.id, title);
+  }
+
   return (
     <li
       key={props.todo.id}
       className={`${styles.listItem} ${props.todo.isDone ? styles.listItemChecked : ""}`}
     >
-      <div className={styles.todoLeft}>
-        <label className={styles.checkbox}>
-          <input
-            type="checkbox"
-            onChange={() => changeTodoStatus(props.todo.id, props.todo.isDone)}
-            checked={props.todo.isDone}
-          />
-          <span className={styles.checkboxMark}></span>
-        </label>
-        <div className={styles.todoInfo}>
-          <input
-            className={`${styles.todoTitle} ${props.todo.isDone ? styles.titleChecked : ""}`}
-            value={title}
-            onChange={(event) => setTitle(event.currentTarget.value)}
-            disabled={!isEditing}
-          ></input>
-          {errorText !== "" ? (
-            <span className={styles.error}>{errorText}</span>
-          ) : (
-            ""
-          )}
-          <span className={styles.todoDate}>{props.todo.created}</span>
+      <form onSubmit={(event) => handleTitleFormSubmit(event)}>
+        <div className={styles.todoLeft}>
+          <label className={styles.checkbox}>
+            <input
+              type="checkbox"
+              onChange={() =>
+                changeTodoStatus(props.todo.id, props.todo.isDone)
+              }
+              checked={props.todo.isDone}
+            />
+            <span className={styles.checkboxMark}></span>
+          </label>
+          <div className={styles.todoInfo}>
+            <input
+              className={`${styles.todoTitle} ${props.todo.isDone ? styles.titleChecked : ""}`}
+              value={title}
+              onChange={(event) => setTitle(event.currentTarget.value)}
+              disabled={!isEditing}
+            ></input>
+            {errorText !== "" ? (
+              <span className={styles.error}>{errorText}</span>
+            ) : (
+              ""
+            )}
+            <span className={styles.todoDate}>{props.todo.created}</span>
+          </div>
         </div>
-      </div>
 
-      <div className={styles.btns}>
-        {isEditing ? (
-          <>
+        <div className={styles.btns}>
+          {isEditing ? (
+            <>
+              <button
+                className={`${styles.todoBtn} ${isEditing ? styles.activeBtn : ""}`}
+                disabled={props.todo.isDone}
+                type="submit"
+              >
+                Сохранить
+              </button>
+              <button
+                className={`${styles.todoBtn} ${styles.cancelBtn}`}
+                onClick={() => onCancelEdit()}
+                type="button"
+              >
+                Отмена
+              </button>
+            </>
+          ) : (
             <button
               className={`${styles.todoBtn} ${isEditing ? styles.activeBtn : ""}`}
-              onClick={() => handleSaveTodo(props.todo.id, title)}
+              onClick={() => {
+                setIsEditing(true);
+              }}
               disabled={props.todo.isDone}
+              type="button"
             >
-              Сохранить
+              Редактировать
             </button>
-            <button
-              className={`${styles.todoBtn} ${styles.cancelBtn}`}
-              onClick={() => onCancelEdit()}
-            >
-              Отмена
-            </button>
-          </>
-        ) : (
-          <button
-            className={`${styles.todoBtn} ${isEditing ? styles.activeBtn : ""}`}
-            onClick={() => {
-              setIsEditing(true);
-            }}
-            disabled={props.todo.isDone}
-          >
-            Редактировать
-          </button>
-        )}
+          )}
 
-        <button
-          className={`${styles.todoBtn} ${styles.deleteBtn}`}
-          onClick={() => {
-            handleDeleteTodo(props.todo.id);
-          }}
-        >
-          Удалить
-        </button>
-      </div>
+          <button
+            className={`${styles.todoBtn} ${styles.deleteBtn}`}
+            onClick={() => {
+              handleDeleteTodo(props.todo.id);
+            }}
+            type="button"
+          >
+            Удалить
+          </button>
+        </div>
+      </form>
     </li>
   );
 };
