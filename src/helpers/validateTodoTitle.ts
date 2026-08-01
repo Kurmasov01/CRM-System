a183b4a9127cb9304792d203696b0f34424d4152
kurmasov01 <kurmasov01@mail.ru>
2026-08-01 16:59:52 +0500

    ED-2199 / переименовал константы

--- src/helpers/validateTodoTitle.ts
+++ src/helpers/validateTodoTitle.ts
@@ -1,18 +1,18 @@
 export function validateTodoTitle(title: string | undefined): string {
   const value = title?.trim();
 
-  const titleMaxLength:number = 64;
-  const titleMinLength:number = 2;
+  const TITLE_MAX_LENGTH:number = 64;
+  const TITLE_MIN_LENGTH:number = 2;
 
   if (!value) {
     return "Это поле не может быть пустым";
   }
 
-  if (value.length < titleMinLength) {
+  if (value.length < TITLE_MIN_LENGTH) {
     return "Минимальная длина текста 2 символа";
   }
 
-  if (value.length > titleMaxLength) {
+  if (value.length > TITLE_MAX_LENGTH) {
     return "Максимальная длина текста 64 символа";
   }
 
